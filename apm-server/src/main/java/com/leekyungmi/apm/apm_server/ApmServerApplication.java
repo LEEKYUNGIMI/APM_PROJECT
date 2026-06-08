f2e34229a3f7c5677bd55a6564a56fc9769d43f7
kyungmi <lkmba91@gmail.com>
2026-06-08 20:51:16 +0900

    프로젝트 초기세팅

---
+++ apm-server/src/main/java/com/leekyungmi/apm/apm_server/ApmServerApplication.java
@@ -0,0 +1,13 @@
+package com.leekyungmi.apm.apm_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmServerApplication.class, args);
+	}
+
+}
---
+++ apm-server/src/test/java/com/leekyungmi/apm/apm_server/ApmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leekyungmi.apm.apm_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
